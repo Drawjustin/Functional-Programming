2b716bcef89826704fb99adbd08cccd91a368e6b
유현종 <guswhd903@naver.com>
2025-09-06 15:49:30 +0900

    김영한 고급 자바3 7일차

---
+++ java-adv3/src/stream/collectors/Collectors2Map.java
@@ -0,0 +1,48 @@
+package stream.collectors;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Collectors2Map {
+    public static void main(String[] args) {
+        Map<String, Integer> map1 = Stream.of("Apple", "Banana", "Tomato")
+                .collect(Collectors.toMap(
+                        name -> name, // key
+                        name -> name.length() // value
+                ));
+        System.out.println("map1 = " + map1);
+
+
+        // 키 중복 예외: IllegalStateException: Duplicate key Apple
+/*
+        Map<String, Integer> map2 = Stream.of("Apple", "Apple", "Tomato")
+                .collect(Collectors.toMap(
+                        name -> name, // key
+                        name -> name.length() // value
+                ));
+        System.out.println("map2 = " + map2);
+*/
+
+        // 키 중복 대안 (병합)
+        Map<String, Integer> map3 = Stream.of("Apple", "Apple", "Tomato")
+                .collect(Collectors.toMap(
+                        name -> name, // key
+                        name -> name.length(), // value
+                        (oldVal, newVal) -> newVal + oldVal
+                ));
+        System.out.println("map3 = " + map3);
+
+        // Map의 타입 지정
+
+        Map<String, Integer> map4 = Stream.of("Apple", "Apple", "Tomato")
+                .collect(Collectors.toMap(
+                        name -> name, // key
+                        name -> name.length(), // value
+                        (oldVal, newVal) -> newVal + oldVal,
+                        LinkedHashMap::new
+                ));
+        System.out.println("map4 = " + map4.getClass());
+    }
+}
