d40d3bb4f80b6278a995abcaca8645937ba4ccd1
유현종 <guswhd903@naver.com>
2025-09-14 21:26:48 +0900

    김영한 고급 자바 3 완

---
+++ java-adv3/src/functional/SideEffectMain3.java
@@ -0,0 +1,15 @@
+package functional;
+
+import java.util.function.Function;
+
+public class SideEffectMain3 {
+
+
+    public static void main(String[] args) {
+        Function<Integer, Integer> func = x -> x * 2;
+        int x = 10;
+
+        Integer result = func.apply(10);
+        System.out.println("result = " + result);
+    }
+}
